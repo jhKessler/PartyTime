9c14a0ecfcc255670283ccdaed3e2c52144d40a0
Lenny <lenny.bakkalian@gmail.com>
2021-04-14 20:02:13 +0200

    add number formatting

--- frontend/src/app/stats-page/main-page.component.ts
+++ frontend/src/app/stats-page/main-page.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {StatsService} from "../stats.service";
 import {ChartOptions, ChartType} from "chart.js";
 import * as moment from "moment";
+import * as HRN from 'human-readable-numbers'
 
 @Component({
   selector: 'app-stats-page',
@@ -23,7 +24,10 @@ export class MainPageComponent {
         gridLines: {display: false},
         ticks: {
           min: 0,
-          fontColor: 'rgb(210,210,210)'
+          fontColor: 'rgb(210,210,210)',
+          callback(value: number | string, index: number, values: number[] | string[]): string | number | null | undefined {
+            return HRN.toHumanString(value);
+          }
         }
       }]
     },
@@ -31,6 +35,21 @@ export class MainPageComponent {
       display: true,
       labels: {
         fontColor: 'white'
+      }
+    },
+    tooltips: {
+      callbacks: {
+        title(item: Chart.ChartTooltipItem[], data: Chart.ChartData): string | string[] {
+          const label = item[0].label
+          const weeknumber = parseInt(label.split('-')[0]);
+          const year = parseInt(label.split('-')[1]);
+          return moment(new Date(year, 0, (1 + weeknumber - 1) * 7)).format('DD.MM.YYYY') + ' - ' + moment(new Date(year, 0, ((1 + weeknumber - 1) * 7) + 7)).format('DD.MM.YYYY');
+        },
+        afterLabel(tooltipItem: Chart.ChartTooltipItem, data: Chart.ChartData): string | string[] {
+          return [
+            'Formatiert: ' + HRN.toHumanString(tooltipItem.value)
+          ];
+        }
       }
     },
     defaultColor: 'red'
