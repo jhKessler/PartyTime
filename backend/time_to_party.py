687a2d9280ce0a2b99ae44667ae3455c903953bb
Johnny Kessler <68814636+jhKessler@users.noreply.github.com>
2021-04-24 16:13:45 +0200

    small syntax changes

--- backend/time_to_party.py
+++ backend/time_to_party.py
@@ -8,7 +8,10 @@ from database import save_history
 
 def main():
     """write vaccination data to json file"""
+
+    # load data
     data = load_data()
+
     # convert date column to datetime objects
     data["date"] = pd.to_datetime(data["date"], format="%Y-%m-%d")
 
@@ -18,16 +21,23 @@ def main():
 
     # get unique id for week and year
     data["unique_week_nr"] = data["date"].dt.strftime('%U-%Y')
+
     # get vaccinations per week
     nach_woche = dict(data.groupby("unique_week_nr")["dosen_kumulativ"].max())
     nach_woche = sorted(nach_woche.items(), key=sort_fn)
+
     # format list dtype
     for i in range(len(nach_woche)):
         nach_woche[i] = list(nach_woche[i])
         nach_woche[i][1] = int(nach_woche[i][1])
+
     # drop last week if its not complete yet
     this_week = data[data["unique_week_nr"] == nach_woche[-1][0]]
     vaccinations_this_week = int(this_week["dosen_differenz_zum_vortag"].sum())
+    if len(this_week) < 7:
+        # remove incomplete row from data
+        data = data[data["unique_week_nr"] != nach_woche[-1][0]]
+        del nach_woche[-1]
 
     # misc stats
     einw = scrape_inhabitants()
@@ -37,28 +47,24 @@ def main():
     verabreicht = data["dosen_kumulativ"].max()
     impfdosen_übrig = impfdosen_insgm - verabreicht
 
-    if len(this_week) < 7:
-        data = data[data["unique_week_nr"] != nach_woche[-1][0]]
-        del nach_woche[-1]
-    # remove incomplete row from data
-    
-    # unpack tuple of shape [*, 2] to two 1d arrays
     wochen, nach_woche = zip(*nach_woche)
+
     # get vaccinations of last 7 days
     last_seven_days = data.iloc[-7:]
     last_seven_days_total = last_seven_days["dosen_differenz_zum_vortag"].sum()
     last_seven_days_avg = last_seven_days_total // 7
 
-
     # find best fit line to estimate vaccination progression
     coeffs = np.polyfit(range(len(nach_woche)), nach_woche, deg=2)
     polyn = np.poly1d(coeffs)
+
     # forecast with best fit line
     best_fit_func = []
     best_fit_func_weeks = []
     week = int(wochen[0].split("-")[0]) + 1
     year = int(wochen[0].split("-")[1])
     week_cnt = isoweek.Week.last_week_of_year(year).week
+
     # predict months until herd immunity count is met
     for i in range(100):
         line_val = int(polyn(i))
@@ -71,6 +77,7 @@ def main():
             week_cnt = isoweek.Week.last_week_of_year(year).week
         if line_val > impfdosen_insgm:
             break
+
     # get start date and end date of each week
     week_start = data.groupby("unique_week_nr")["date"].min().dt.strftime("%d.%m.%Y")
     week_end = data.groupby("unique_week_nr")["date"].max().dt.strftime("%d.%m.%Y")
@@ -85,6 +92,7 @@ def main():
         last_week_start = last_week_start + datetime.timedelta(days=7)
         last_week_end = last_week_start + datetime.timedelta(days=6)
         week_str.append(last_week_start.strftime("%d.%m.%Y") + " - " + last_week_end.strftime("%d.%m.%Y"))
+
     # make prediction on when herd immunity is reached (2 weeks for effect to kick in)
     alle_geimpft = (datetime.datetime.strptime(best_fit_func_weeks[-1] + "-1", "%U-%Y-%w") + datetime.timedelta(days=14)).strftime("%Y-%m-%d")
     # save data to json
@@ -108,7 +116,7 @@ def main():
         "impdosen_verabreicht_diese_woche": vaccinations_this_week,
         "week_start_end" : week_str
     }
-
-    save_history(scrape_status_date(), data_dict)
+    update_time = scrape_status_date()
+    save_history(update_time, data_dict)
 
 main()
